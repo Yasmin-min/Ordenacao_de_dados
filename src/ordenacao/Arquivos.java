f74380dbdae88d064ff2e9c04475f745fe1767ed
Yasmin Neumann <yasminneumann@hotmail.com>
2024-09-09 21:58:03 -0300

    Primeiro commit

---
+++ src/ordenacao/Arquivos.java
@@ -0,0 +1,149 @@
+package ordenacao;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class Arquivos {
+	
+	public static void criarPasta() {
+		String caminhoPasta = "C:\\TesteArquivo"; // Substitua pelo caminho e nome da pasta desejada
+	    
+	    // Criando um objeto File com o caminho especificado
+	    File pasta = new File(caminhoPasta);
+
+	    // Verifica se a pasta já existe
+	    if (!pasta.exists()) {
+	        // Cria a pasta
+	        if (pasta.mkdirs()) {
+	            System.out.println("Pasta criada com sucesso: " + pasta.getAbsolutePath());
+	        } else {
+	            System.out.println("Falha ao criar a pasta.");
+	        }
+	    } else {
+	        System.out.println("A pasta já existe.");
+	    }
+	}
+	
+	public static void escritorRandom(String path) throws IOException {
+		BufferedWriter buffWrite = new BufferedWriter(new FileWriter(path));
+		for(int i = 0; i < 100; i++) {
+			double num = Math.random() * 500;
+			int randomNum = (int)num;
+			buffWrite.append(randomNum + "\n");
+		}
+		buffWrite.close();
+	}
+	public static void escritorOrdenado(String path2) throws IOException {
+		BufferedWriter buffWrite = new BufferedWriter(new FileWriter(path2));
+		ArrayList<Integer> numOrdenado = new ArrayList<Integer>();
+		for(int i = 0; i < 100; i++) {
+			double num = Math.random() * 500;
+			Integer num2 = (int)num;
+			numOrdenado.add(num2);
+		}
+		numOrdenado.sort(null);
+		for(int i = 0; i < 100; i++) {
+		buffWrite.append(numOrdenado.get(i) + "\n");
+		}
+		buffWrite.close();
+	}
+	
+	public static void escritorReverso(String path3) throws IOException {
+		BufferedWriter buffWrite = new BufferedWriter(new FileWriter(path3));
+		ArrayList<Integer> numOrdenado = new ArrayList<Integer>();
+		for(int i = 0; i < 100; i++) {
+			double num = Math.random() * 500;
+			Integer num2 = (int)num;
+			numOrdenado.add(num2);
+		}
+		Collections.sort(numOrdenado, Collections.reverseOrder());
+		
+		for(int i = 0; i < 100; i++) {
+		buffWrite.append(numOrdenado.get(i) + "\n");
+		}
+		buffWrite.close();
+	}
+	
+	public static void escritorRandom10000(String path) throws IOException {
+		BufferedWriter buffWrite = new BufferedWriter(new FileWriter(path));
+		for(int i = 0; i < 10000; i++) {
+			double num = Math.random() * 10000;
+			int randomNum = (int)num;
+			buffWrite.append(randomNum + "\n");
+		}
+		buffWrite.close();
+	}
+	
+	public static void escritorOrdenado10000(String path2) throws IOException {
+		BufferedWriter buffWrite = new BufferedWriter(new FileWriter(path2));
+		ArrayList<Integer> numOrdenado = new ArrayList<Integer>();
+		for(int i = 0; i < 10000; i++) {
+			double num = Math.random() * 10000;
+			Integer num2 = (int)num;
+			numOrdenado.add(num2);
+		}
+		numOrdenado.sort(null);
+		for(int i = 0; i < 10000; i++) {
+		buffWrite.append(numOrdenado.get(i) + "\n");
+		}
+		buffWrite.close();
+	}
+	public static void escritorReverso10000(String path3) throws IOException {
+		
+		BufferedWriter buffWrite = new BufferedWriter(new FileWriter(path3));
+		ArrayList<Integer> numOrdenado = new ArrayList<Integer>();
+		for(int i = 0; i < 10000; i++) {
+			double num = Math.random() * 10000;
+			Integer num2 = (int)num;
+			numOrdenado.add(num2);
+		}
+		Collections.sort(numOrdenado, Collections.reverseOrder());
+		
+		for(int i = 0; i < 10000; i++) {
+		buffWrite.append(numOrdenado.get(i) + "\n");
+		}
+		buffWrite.close();
+	}
+	
+	public static void leitor(String path) throws IOException {
+		BufferedReader buffRead = new BufferedReader(new FileReader(path));
+		String linha = buffRead.readLine();
+		Integer vetor[] = new Integer[100];
+		int i = 0;
+		while (true) {
+			if (linha != null) {
+				vetor[i] = Integer.parseInt(linha);
+				i++;
+				System.out.print(vetor[i-1]+ "; ");
+			} else
+				break;
+			linha = buffRead.readLine();
+		}
+		buffRead.close();
+	}
+	public static void leitor10000(String path) throws IOException {
+		BufferedReader buffRead = new BufferedReader(new FileReader(path));
+		String linha = "0";
+		Integer vetor[] = new Integer[10001];
+		int i = 0;
+		while (true) {
+			if (linha != null) {
+				vetor[i] = Integer.parseInt(linha);
+				i++;
+				System.out.println(vetor[i-1]);
+
+			} else
+				break;
+			linha = buffRead.readLine();
+		}
+		buffRead.close();
+	}
+	
+}
---
+++ src/ordenacao/TestarOrdenacao.java
@@ -0,0 +1,84 @@
+package ordenacao;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import dados.Item;
+//import jdk.jshell.SourceCodeAnalysis;
+
+public class TestarOrdenacao {
+
+	public static void main(String[] args) throws IOException{
+		Vetor vetor = new Vetor(100);
+		Scanner scan = new Scanner(System.in);
+		
+		Arquivos.criarPasta();
+		
+		String path = "C:\\TesteArquivo\\texto.txt";
+		String path2 = "C:\\TesteArquivo\\texto2.txt";
+		String path3 = "C:\\TesteArquivo\\texto3.txt";
+		
+		String path4 = "C:\\TesteArquivo\\texto4.txt";
+		String path5 = "C:\\TesteArquivo\\texto5.txt";
+		String path6 = "C:\\TesteArquivo\\texto6.txt";
+		
+		
+		
+		Arquivos.escritorRandom(path);
+		Arquivos.escritorOrdenado(path2);
+		Arquivos.escritorReverso(path3);
+		Arquivos.leitor(path);
+		TestarOrdenacao.leitor(path, vetor);
+		
+		vetor.heapSort();
+		System.out.println("Num. Comparacoes: \n" + vetor.contComparacaoHeap() + "\nVetor ordenado: \n" + vetor.toString());
+		//Arquivos.escritorReverso(path3);
+		//Arquivos.escritorRandom10000(path4);
+		//Arquivos.escritorOrdenado10000(path5);
+		//Arquivos.escritorReverso10000(path6);
+	}
+		
+		public static void leitor(String path, Vetor vetor) throws IOException {
+			BufferedReader buffRead = new BufferedReader(new FileReader(path));
+			String linha = buffRead.readLine();
+			Integer vetorLocal[] = new Integer[100];
+			int i = 0;
+			while (true) {
+				if (linha != null) {
+					vetorLocal[i] = Integer.parseInt(linha);
+					vetor.inserirDados(new Item(vetorLocal[i]));
+					i++;
+					//System.out.println(vetorLocal[i-1]);
+				} else {
+					break;
+				}
+				
+				linha = buffRead.readLine();
+			}
+			buffRead.close();
+		}
+		
+		
+		
+		
+		
+		
+		
+		//entrada de dados
+		/*for (int i=0; i< 10; i++) {
+			System.out.println("Digite um n�mero inteiro: ");
+			vetor.inserirDados(new Item(scan.nextInt())) ;
+		}
+		System.out.println("vetor desordenado\n"+vetor.toString());
+
+//coloque aqui o metodo a ser testado
+		vetor.heapSort();
+
+//resultado
+		System.out.println("\nvetor ordenado\n"+vetor.toString());*/
+	}
+
+	
+
+
